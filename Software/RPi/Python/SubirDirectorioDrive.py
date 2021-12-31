570b8486c144621364a56db2d3732f824758b8e6
Rodrig087 <miltonho@hotmail.com>
2021-12-31 17:16:08 -0500

    Se implemento el programa SubirDirectorioDrive.py para subir los registros y los eventos extraidos a Google Drive.

---
+++ Software/RPi/Python/SubirDirectorioDrive.py
@@ -0,0 +1,251 @@
+from __future__ import print_function
+from googleapiclient import errors
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+import os
+
+from datetime import datetime
+import time
+
+
+global objLogFile
+global pathDriveID
+global service
+    
+# ///////////////////////////////// Archivos //////////////////////////////////
+
+pathDirectorioArchivos = '/home/rsa/Resultados/EventosExtraidos/'
+
+pathArchivosConfiguracion = '/home/rsa/Configuracion/'
+pathLogFiles = '/home/rsa/LogFiles/'
+
+archivoDatosConfiguracion = pathArchivosConfiguracion + 'DatosConfiguracion.txt'
+#archivoNombresArchivosDirectorio 
+
+credentialsFile = pathArchivosConfiguracion + 'credentials.json'
+tokenFile = pathArchivosConfiguracion + 'token.json'
+
+# /////////////////////////////////////////////////////////////////////////////
+
+
+
+# ////////////////////////////////// Metodos //////////////////////////////////
+
+
+# **********************************************************************
+# ********************* Programa Principal *****************************
+# **********************************************************************
+def programaPrincipal ():
+    
+    global pathDriveID 
+    global objLogFile
+    global service
+        
+    # If modifying these scopes, delete the file token.json.
+    SCOPES = 'https://www.googleapis.com/auth/drive'
+    # Llama al metodo para intentar conectarse a Google Drive
+    service = Try_Autenticar_Drive(SCOPES)
+     
+    # Fecha actual
+    fechaActual = datetime.now()
+    fechaFormato = fechaActual.strftime('%Y-%m-%d') 
+    
+    #Recupera la carpeta
+    ficheroConfiguracion = open(archivoDatosConfiguracion)
+    lineasFicheroConfiguracion = ficheroConfiguracion.readlines()
+    pathDriveID = lineasFicheroConfiguracion[7].rstrip('\n')
+    
+    # Crea el archivo para almacenar los logs del proyectos, que eventos ocurren
+    objLogFile = pathLogFiles + 'Log' + lineasFicheroConfiguracion[0].rstrip('\n') + fechaFormato + '.txt'
+    print(objLogFile)
+    # Llama al metodo para crear un nuevo archivo log
+    #guardarDataInLogFile ("Inicio")
+    
+    #Lista los archivos del directorio a subir
+    listaArchivos = os.listdir(pathDirectorioArchivos)
+    listaArchivosOrdenada = sorted(listaArchivos)
+    
+    #print(listaArchivosOrdenada)
+        
+    dicArchivosEnviados = subirArchivosDrive (listaArchivosOrdenada)  
+    
+# **********************************************************************
+# ******************* Fin Programa Principal ***************************
+# **********************************************************************    
+
+
+# **********************************************************************
+# ******************* Metodo get_authenticated *************************
+# **********************************************************************
+# Metodo que permite realizar la autenticacion a Google Drive, para esto
+# es necesario tener el archivo credentials.json en la misma carpeta de
+# este script. La primera vez se abrira el navegador para realizar la
+# autenticacion y se creara el archivo token.json que permite realizar
+# directamente la autenticacion desde la segunda vez que se ejecute, sin
+# necesidad del navegador
+def get_authenticated(SCOPES, credential_file = credentialsFile,
+                  token_file = tokenFile, service_name = 'drive',
+                  api_version = 'v3'):
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    store = file.Storage(token_file)
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets(credential_file, SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build(service_name, api_version, http = creds.authorize(Http()))
+
+    return service
+# **********************************************************************
+# ****************** Fin Metodo get_authenticated **********************
+# **********************************************************************
+
+
+# **********************************************************************
+# ************************* Metodo insert_file *************************
+# **********************************************************************
+# Metodo que permite subir un archivo a la cuenta de Drive, no importa
+# el tamaño del archivo. Si se desea subir a una carpeta en específico
+# se debe colocar el parent_id
+def insert_file(service, name, description, parent_id, mime_type, filename):
+    """Insert new file.
+
+    Args:
+        service: Drive API service instance.
+        name: Name of the file to insert, including the extension.
+        description: Description of the file to insert.
+        parent_id: Parent folder's ID.
+        mime_type: MIME type of the file to insert.
+        filename: Filename of the file to insert.
+    Returns:
+        Inserted file metadata if successful, None otherwise.
+    """
+    #media_body = MediaFileUpload(filename, mimetype = mime_type, resumable = False, chunksize=256 * 1024)
+    #media_body = MediaFileUpload(filename, mimetype = mime_type, resumable = False)
+    media_body = MediaFileUpload(filename, mimetype = mime_type, chunksize=-1, resumable = True)
+    body = {
+        'name': name,
+        'description': description,
+        'mimeType': mime_type
+    }
+        
+    # Si se recibe la ID de la carpeta superior, la coloca
+    if parent_id:
+        body['parents'] = [parent_id]
+
+    # Realiza la carga del archivo en la carpeta respectiva de Drive
+    try:
+        #print("punto de control")
+        file = service.files().create(
+            body = body,
+            media_body = media_body,
+            fields='id').execute()
+                
+        return file
+        #print "Upload Complete!"
+    
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+        return None
+# **********************************************************************
+# *********************** Fin Metodo insert_file ***********************
+# **********************************************************************
+
+
+# **********************************************************************
+# Metodo para intentar conectarse a Google Drive y activar la bandera de conexion
+# **********************************************************************
+def Try_Autenticar_Drive(SCOPES):
+    global isConecctedDrive
+    # Llama al metodo para realizar la autenticacion, la primera vez se
+    # abrira el navegador, pero desde la segunda ya no
+    try:
+        service = get_authenticated(SCOPES)
+        isConecctedDrive = True
+        print("Inicio Drive Ok")
+        #guardarDataInLogFile ("Inicio Drive Ok")
+        return service
+    except:
+        isConecctedDrive = False
+        print("********** Error Inicio Drive ********")
+        #guardarDataInLogFile ("Error Inicio Drive")
+        return 0
+# **********************************************************************
+# Fin del metodo para conectarse a Drive
+# **********************************************************************
+
+
+# **********************************************************************
+# ********************* Metodo subirArchivosDrive **********************
+# **********************************************************************
+# Metodo que sube los archivos descargados por los nodos al Drive
+def subirArchivosDrive (listaNombresArchivosNodos):
+    global service
+    global objLogFile
+
+    print ('')
+    print ('Comienza subida de archivos al Drive')
+    fechaActual = datetime.now()
+    fechaFormato = fechaActual.strftime('%Y-%m-%d-%H-%M-%S')
+    print (fechaFormato)
+
+    if len(listaNombresArchivosNodos) > 0:
+        print ('Total archivos ' + str(len(listaNombresArchivosNodos)))
+    else:
+        print ('No hay archivos para subir')
+
+    # Recorre la lista de nombres de archivos de los nodos que debieron crearse
+    for nombreArchivo in listaNombresArchivosNodos:
+        # Agrega el path de resultados
+        pathTotal = pathDirectorioArchivos + nombreArchivo
+        print ('Archivo ', pathTotal)
+        # Llama al metodo para subir el archivo a Google Drive
+        try:
+            # Llama al metodo para guardar el evento ocurrido en el archivo
+            print('Subiendo el archivo: %s' %pathTotal)
+            #guardarDataInLogFile ("Subiendo el archivo: " + nombreArchivo)
+            file_uploaded = insert_file(service, nombreArchivo, nombreArchivo, pathDriveID, 'text/x-script.txt', pathTotal)
+            #guardarDataInLogFile ("Archivo subido correctamente a Google Drive " + str(file_uploaded))
+            print('Archivo' + nombreArchivo + ' subido correctamente a Google Drive ' )
+        except:
+            # Llama al metodo para guardar el evento ocurrido en el archivo
+            #guardarDataInLogFile ("Error subiendo el archivo a Google Drive")
+            print ('Error subiendo el archivo a Google Drive')
+# **********************************************************************
+# ***************** Fin Metodo subirArchivosDrive **********************
+# **********************************************************************
+
+
+# **********************************************************************
+# ****************** Metodo guardarDataInLogFile ***********************
+# **********************************************************************
+def guardarDataInLogFile (info):
+    global objLogFile
+
+    timeActual = datetime.now()
+    timeFormato = timeActual.strftime('%Y-%m-%d %H:%M:%S')
+
+    # Abre o crea el nuevo archivo de texto y en formato para escribir
+    archivo = open(objLogFile, "a")
+    archivo.write((timeFormato + "\t" + info + "\n"))
+    archivo.close()
+# **********************************************************************
+# *************** Fin Metodo guardarDataInLogFile **********************
+# **********************************************************************
+
+# /////////////////////////////////////////////////////////////////////////////
+
+
+
+
+# ///////////////////////////////// Principal /////////////////////////////////
+
+if __name__ == '__main__':
+    programaPrincipal ()
+    
+     
+# /////////////////////////////////////////////////////////////////////////////
